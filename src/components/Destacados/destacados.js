c2e13f2154cd6e0bcd04270bce8e79d3c6a54ec1
CAMI <camimorixe@gmail.com>
2022-11-16 13:56:09 -0300

    cambios

--- src/components/Destacados/destacados.js
+++ src/components/Destacados/destacados.js
@@ -1,56 +1,58 @@
 import React from "react"
-import { m2, producto2, producto3, producto4, producto5, producto6, producto7} from "../../assets"
+import { m2, producto2, producto3, producto4} from "../../assets"
 import './destacados.css'
 
 export const Destacados = () =>{
     return(
         <section id="productos">
-        <div class="container text-center mt-5 py-5">
+        <div className="container text-center mt-5 py-5 d-flex">
           <h3>Nuestros productos destacados <br />
-            <small class="text-muted">Lo que eligen los argentinos.</small></h3>
+            <small className="text-muted">Lo que eligen los argentinos.</small></h3>
         </div>
-        <div class="container">
-          <div class="row row-cols-4 row-cols-md-4">
-            <div class="col">
-              <div class="card">
-                <img src={m2} class="card-img-top" />
-                <div class="card-body">
-                  <h5 class="card-title">MATE DELUXE DE ALGARROBO -LISO-</h5>
-                  <p class="card-text"><small class="text-muted"><del>$10.640,00</del></small><strong> $7.980,00</strong> <br />
+        <div className="container">
+          <div className="destacados">
+          <div className="row row-cols-4 row-cols-md-4">
+            <div className="col">
+              <div className="card">
+                <img src={m2} className="card-img-top"/>
+                <div className="card-body">
+                  <h5 className="card-title">MATE DELUXE DE ALGARROBO -LISO-</h5>
+                  <p className="card-text"><small className="text-muted d-flex"><del>$10.640,00</del></small><strong> $7.980,00</strong> <br />
                     3 cuotas sin interés de <strong>$2.660,00</strong></p>
                 </div>
               </div>
             </div>
-            <div class="col">
-              <div class="card">
+            <div className="col">
+              <div className="card">
                 <img src={producto2} />
-                <div class="card-body">
-                  <h5 class="card-title">MATE IMPERIAL PREMIUM</h5>
-                  <p class="card-text"><small class="text-muted"><del>$11.500,00</del></small><strong> $9.900,00</strong> <br />
+                <div className="card-body">
+                  <h5 className="card-title">MATE IMPERIAL PREMIUM</h5>
+                  <p className="card-text"><small className="text-muted d-flex"><del>$11.500,00</del></small><strong> $9.900,00</strong> <br />
                     3 cuotas sin interés de <strong>$3.500,00</strong></p>
                 </div>
               </div>
             </div>
-            <div class="col">
-              <div class="card">
+            <div className="col">
+              <div className="card">
                 <img src={producto3} />
-                <div class="card-body">
-                  <h5 class="card-title">MATE PAMPA</h5>
-                  <p class="card-text"><small class="text-muted"><del>$9.500,00</del></small><strong> $7.400,00</strong> <br />
+                <div className="card-body">
+                  <h5 className="card-title">MATE PAMPA</h5>
+                  <p className="card-text"><small className="text-muted d-flex"><del>$9.500,00</del></small><strong> $7.400,00</strong> <br />
                     3 cuotas sin interés de <strong>$2.550,00</strong></p>
                 </div>
               </div>
             </div>
-            <div class="col">
-              <div class="card">
+            <div className="col">
+              <div className="card">
                 <img src={producto4} />
-                <div class="card-body">
-                  <h5 class="card-title">MATE BLANCO LISO</h5>
-                  <p class="card-text"><small class="text-muted"><del>$12.500,00</del></small><strong> $10.400,00</strong> <br />
+                <div className="card-body">
+                  <h5 className="card-title">MATE BLANCO LISO</h5>
+                  <p className="card-text"><small className="text-muted d-flex"><del>$12.500,00</del></small><strong> $10.400,00</strong> <br />
                     3 cuotas sin interés de <strong>$3.700,00</strong></p>
                 </div>
               </div>
             </div>
+          </div>
           </div>
         </div>
       </section>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,77 +1,66 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './footer.css'
+import { whatsapp, instagram, facebook, youtube } from '../../assets'
 
 
 
 
 export const Footer = () => {
   return (
-    <footer className='text-white py-4 bg-dark'>
+    <footer className='text-white py-4' id="footer">
         <div className="container">
             <div className="row gy-3">
                 <div className="col-lg-3 col-md-6 d-flex">
-                    <i className="bi bi-geo-alt icon"></i>
-                    <div>
-                        <h4>Direccion</h4>
-                        <p>
-                        Maipu 864, CABA <br/>
-                        </p>
-                        <p>
-                        Buenos Aires, Argentina <br/>
-                        </p>
-                    </div>
 
-                </div>
-
-                <div className="col-lg-3 col-md-6 footer-links d-flex">
-                    <i className="bi bi-telephone icon"></i>
                     <div>
-                        <h4>Menu</h4>
-                        <ul className="nav flex-column" id="menu">
-                        <li className="nav-item mb-2"><Link
+                      <h4>Menu</h4>
+                      <ul class="nav flex-column" id="menu">
+                        <li class="nav-item mb-2"><Link
                         to="inicio">
-                        Inicio
+                        <a>Inicio</a>
                         </Link>
                         </li>
-
-                        <li className="nav-item mb-2"><Link
+                        <li class="nav-item mb-2"><Link
                         to="productos">
                         Productos
                         </Link>
                         </li>
-
-                        <li className="nav-item mb-2"><Link
+                        <li class="nav-item mb-2"><Link
                         to="contacto">
                         Contacto
                         </Link>
                         </li>
-                        </ul>
+                      </ul>
                     </div>
+
                 </div>
 
                 <div className="col-lg-3 col-md-6 footer-links d-flex">
-                    <i className="bi bi-clock icon"></i>
                     <div>
-                        <h4>Abierto de</h4>
-                        <p>
-                        <strong>Lun-Sab:</strong>  11AM - 23PM<br/>
-                        Domingos: Cerrado
-                        </p>
+                      <h4>Contacto</h4>
+                      <ul class="nav flex-column ">
+                        <li class="nav-item mb-2">matemania@gmail.com</li>
+                        <li class="nav-item mb-2">4456-7888 </li>
+                        <li class="nav-item mb-2">Córdoba 347, CABA</li>
+                      </ul>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 footer-links">
-                  <h4>Seguinos</h4>
-                  <div class="social-links d-flex justify-content-between">
-                    <a href="https://wa.me/542944924649" class="twitter"><i class="bi bi-whatsapp" href="https://wa.me/542944924649"></i></a>
-                    <a href="https://www.facebook.com/CheMateAr?_ga=2.88231707.1203844562.1667247646-669339932.1662398604&_gac=1.84163307.1667255571.CjwKCAjw5P2aBhAlEiwAAdY7dJ-ituxj7HhPoK6m24iAytYAevFD6qb_W1hnttgnZaaCXgbYmqfRNBoCcRIQAvD_BwE" class="facebook"><i class="bi bi-facebook" href="https://wa.me/542944924649"></i></a>
-                    <a href="https://www.instagram.com/che.mate/?_ga=2.88231707.1203844562.1667247646-669339932.1662398604&_gac=1.84163307.1667255571.CjwKCAjw5P2aBhAlEiwAAdY7dJ-ituxj7HhPoK6m24iAytYAevFD6qb_W1hnttgnZaaCXgbYmqfRNBoCcRIQAvD_BwE" class="instagram"><i class="bi bi-instagram"></i></a>
-                    <a href="https://www.youtube.com/watch?v=7tmDMPiu_cM&ab_channel=SudamericanMate" class="linkedin"><i class="bi bi-youtube"></i></a>
+
+              <div className="col-lg-3 col-md-6 footer-links d-flex">
+                <div className="social-links d-flex justify-content-between">
+                  <ul class="list-unstyled d-flex" id="fotos_redes">
+                    <li class="ms-3"><a class="link-dark" href="https://wa.me/542944924649"><img src = {whatsapp} class="img_footer" width="57px"/></a></li>
+                    <li class="ms-3"><a class="link-dark" href="https://www.facebook.com/CheMateAr?_ga=2.88231707.1203844562.1667247646-669339932.1662398604&_gac=1.84163307.1667255571.CjwKCAjw5P2aBhAlEiwAAdY7dJ-ituxj7HhPoK6m24iAytYAevFD6qb_W1hnttgnZaaCXgbYmqfRNBoCcRIQAvD_BwE"><img src = {facebook} class="img_footer" width="50px"/></a></li>
+                    <li class="ms-3"><a class="link-dark" href="https://www.instagram.com/che.mate/?_ga=2.88231707.1203844562.1667247646-669339932.1662398604&_gac=1.84163307.1667255571.CjwKCAjw5P2aBhAlEiwAAdY7dJ-ituxj7HhPoK6m24iAytYAevFD6qb_W1hnttgnZaaCXgbYmqfRNBoCcRIQAvD_BwE"><img src= {instagram} class="img_footer" width="50px"/></a></li>
+                    <li class="ms-3"><a class="link-dark" href="https://www.youtube.com/watch?v=7tmDMPiu_cM&ab_channel=SudamericanMate"><img src = {youtube} class="img_footer" width="50px"/></a></li>
+                  </ul>
                   </div>
                 </div>
             </div>
         </div>
-
+        <br/>
+        <br/>
         <div className="container text-center">
             <div className="copyright">
                 &copy; Copyright <strong><span>Matemania</span></strong>. Todos los derechos reservados
